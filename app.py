c49e5c94e07e4283b79e2b75b258b067da20b536
Keith Olszewski <kwo20@msstate.edu>
2022-10-02 13:46:22 -0500

    Updated app.py to include posting functionality

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from flask import Flask, render_template, redirect, url_for, request, jsonify
 import json
 import sqlite3
-
+import random
+current_user = "test"
 app = Flask(__name__)
-
 def db_connection():
     conn = None
     try:
@@ -35,6 +35,7 @@ def create():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    
     conn = db_connection()
     cursor = conn.cursor()
     if request.method == 'POST':
@@ -43,10 +44,28 @@ def login():
         if not test:
             return render_template('login.html')
         elif test[0] == request.form['password']:
+            global current_user
+            current_user = request.form['username']
             return render_template('mainpage.html')
         else:
             return render_template('login.html')
-    return render_template('login.html')
+    else:
+        return render_template('login.html')
+
+@app.route('/mainpage', methods=['GET', 'POST'])
+def post():
+    conn = db_connection()
+    cursor = conn.cursor()
+    if request.method == 'POST':
+        new_post = request.form['text']
+        sql = """INSERT INTO posts (content, username)
+                 VALUES (?, ?)"""
+        
+        cursor = cursor.execute(sql, (new_post, current_user))
+        conn.commit()
+        return render_template('mainpage.html')
+    else:
+        return render_template('mainpage.html')
 
 
 if __name__ == "__main__":
